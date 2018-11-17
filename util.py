c6be90999afac5da7eb730d07eef1ce8cdafdece
cristiprg <cristian.prigoana@yahoo.com>
2018-11-17 19:16:59 +0100

    Fix decision layers bug

--- util.py
+++ util.py
@@ -3,10 +3,43 @@ from tensorflow.python.platform import tf_logging as logging
 from hyperopt import hp
 
 import loader
-from train_specgan import _WINDOW_LEN
+# from train_specgan import _WINDOW_LEN
 from specgan import SpecGANDiscriminator
 from wavegan import WaveGANDiscriminator
 
+import pprint
+
+"""
+  Constants
+"""
+_FS = 16000
+_WINDOW_LEN = 16384
+_D_Z = 100
+_CLIP_NSTD = 3.
+_LOG_EPS = 1e-6
+
+class SaveAtEnd(tf.train.SessionRunHook):
+    '''a training hook for saving the final variables'''
+
+    def __init__(self, filename):
+        '''hook constructor
+
+        Args:
+            filename: where the model will be saved
+            variables: the variables that will be saved'''
+
+        self.filename = filename
+        # self.variables = variables
+
+    def begin(self):
+        '''this will be run at session creation'''
+
+        #pylint: disable=W0201
+        self._saver = tf.train.Saver()
+
+    def end(self, session):
+        '''this will be run at session closing'''
+        self._saver.save(session, self.filename, global_step=tf.train.get_or_create_global_step())
 
 def resettable_metric(metric, scope_name, **metric_args):
     '''
@@ -43,8 +76,18 @@ batch_size_choice = [64, 128]
 layers_transferred_choice = [1, 2, 3, 4]
 learning_rate_base_Adam_choice = [0.00001, 0.00003, 0.00005, 0.0001, 0.0003, 0.0005]
 learning_rate_mode_choice = ['constant', 'decaying']
-layer_1_size_choice = [2 ** x for x in range(4, 8)]
-layer_2_size_choice = [2 ** x for x in range(4, 8)]
+
+# Super ugly hack to choose layer_1_size > layer_2_size, and to be able to convert best (returned by hyperopt)
+# back to a json
+case1_layer1_choice = [2 ** x for x in range(3, 11)]
+case2_layer1_choice = [2 ** x for x in range(7, 11)]
+case2_layer2_choice = [2 ** x for x in range(3, 7)]
+
+layers_choice = [
+    ('case 0', (hp.choice('case0_layer1', range(1))), hp.choice('case0_layer2', range(1))),  # no decision layer at all
+    ('case 1', (hp.choice('case1_layer1', case1_layer1_choice)), hp.choice('case1_layer2', range(1))),  # one decision layer
+    ('case 2', (hp.choice('case2_layer1', case2_layer1_choice)), hp.choice('case2_layer2', case2_layer2_choice)),  # two decision layers
+]
 
 def define_search_space():
     space = {
@@ -57,16 +100,7 @@ def define_search_space():
         ]),
 
         'learning_rate_mode': hp.choice('learning_rate_mode', learning_rate_mode_choice),
-
-        'layer_1_size': hp.choice('layer_1_size', layer_1_size_choice),
-
-        'layer_2': hp.choice('layer_2', [
-            False,
-            {
-                'layer_2_size': hp.choice('layer_2_size', layer_2_size_choice)
-            }
-        ]),
-
+        'layers': hp.choice('layers', layers_choice),
         'layers_transferred': hp.choice('layers_transferred', layers_transferred_choice)
     }
 
@@ -81,17 +115,20 @@ def transform_hyperopt_result_to_dict_again(hyperopt_param):
     params['optimizer']['Adam'] ={}
     params['optimizer']['Adam']['learning_rate_base'] = learning_rate_base_Adam_choice[hyperopt_param['learning_rate_base_Adam']]
     params['learning_rate_mode'] = learning_rate_mode_choice[hyperopt_param['learning_rate_mode']]
-    params['layer_1_size'] = layer_1_size_choice[hyperopt_param['layer_1_size']]
-    # params['layer_2'] = False if hyperopt_param['layer_2'] == 0 else True
-    #
-    # if params['layer_2']:
-    #     params['layer_2']['layer_2_size'] = layer_2_size_choice[hyperopt_param['layer_2_size']]
 
-    if hyperopt_param['layer_2'] == 1:  # which means True - check with the choice
-        params['layer_2'] = {}
-        params['layer_2']['layer_2_size'] = layer_2_size_choice[hyperopt_param['layer_2_size']]
+    layers_case = hyperopt_param['layers']
+    if layers_case == 0:
+        layer_1_size, layer_2_size = 0, 0
+    elif layers_case == 1:
+        layer_1_size = case1_layer1_choice[hyperopt_param['case1_layer1']]
+        layer_2_size = 0
+    elif layers_case == 2:
+        layer_1_size = case2_layer1_choice[hyperopt_param['case2_layer1']]
+        layer_2_size = case2_layer2_choice[hyperopt_param['case2_layer2']]
     else:
-        params['layer_2'] = False
+        raise ValueError("Wrong value for layers_case: " + str(layers_case))
+
+    params['layers'] = (layers_case, layer_1_size, layer_2_size)
 
     return params
 
@@ -99,17 +136,31 @@ def transform_hyperopt_result_to_dict_again(hyperopt_param):
 def get_cnn_model(params, x, processing_specgan=True):
     batch_size = params['batch_size']
     layers_transferred = params['layers_transferred']
+    layer_1_size = params['layers'][1]
+    layer_2_size = params['layers'][2]
+
     with tf.variable_scope('D'):
         D_x_training = SpecGANDiscriminator(x) if processing_specgan else WaveGANDiscriminator(x)  # leave the other parameters default
 
         last_conv_op_name = 'D_x/D/Maximum_' + str(layers_transferred)
         last_conv_tensor = tf.get_default_graph().get_operation_by_name(last_conv_op_name).values()[0]
-        last_conv_tensor = tf.reshape(last_conv_tensor, [batch_size, -1])  # Flatten
+        last_conv_tensor = tf.reshape(last_conv_tensor, [batch_size, -1], name="reshape_of_last_transferred_layer")  # Flatten
 
     with tf.variable_scope('decision_layers'):
-        cnn_decision_layer = tf.layers.dense(last_conv_tensor, 10)
+        if layer_1_size == 0:
+            output = last_conv_tensor
+        else:
+            with tf.variable_scope('decision_layer_1'):
+                output = tf.layers.dense(last_conv_tensor, layer_2_size)
 
-    return cnn_decision_layer
+            if layer_2_size != 0:
+                with tf.variable_scope('decision_layer_2'):
+                    output = tf.layers.dense(output, layer_2_size)
+
+        with tf.variable_scope('cnn_output_layer'):
+            output = tf.layers.dense(output, 10)
+
+    return output
 
 
 def get_optimizer(params, D_loss):
